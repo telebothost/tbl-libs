afa111cc7e17301b924651a6e8210060f86c6eb9
Soumyadeep Das <133207548+Soumyadeep765@users.noreply.github.com>
2025-06-17 22:21:12 +0530

    Update pr.js

--- Libs/pr.js
+++ Libs/pr.js
@@ -1,10 +1,10 @@
 let text = "Hii";
 
-const hi = async () => {
-  await Promise.all([
+function hi() {
+  return Promise.all([
     Api.sendMessage({ text }),
     Api.sendMessage({ text })
   ]);
-};
+}
 
 module.exports = { hi };
