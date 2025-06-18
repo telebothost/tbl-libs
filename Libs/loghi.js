e51229b2cb3623aeb9941288c26d7e66498fec99
Soumyadeep Das <133207548+Soumyadeep765@users.noreply.github.com>
2025-06-18 12:06:38 +0530

    Update loghi.js

--- Libs/loghi.js
+++ Libs/loghi.js
@@ -1,11 +1,11 @@
  async function hi() {
   let userId = user.id;
-    let result =  await Api.sendMessage({
+    let result =   Api.sendMessage({
       chat_id: userId, // Replace with real public channel
       text: userId
     });
 
-   await Bot.inspect(result);
+    Bot.sendMessage(result);
 }
 
 module.exports = { hi };
